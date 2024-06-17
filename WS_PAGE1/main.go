98dbf88cd8374645fa264acc58a8c940ad81fad3
pungpondinwza <86757253+dlwlilac@users.noreply.github.com>
2024-06-17 15:16:22 +0700

    go taining

---
+++ WS_PAGE1/main.go
@@ -0,0 +1,165 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"strconv"
+	"strings"
+)
+
+func power(base, exponent float64) float64 {
+	return math.Pow(base, exponent)
+}
+
+func main() {
+
+	// num0()
+	// num1()
+	// num12()
+	// num2()
+	// num3()
+	// num31()
+	// num4()
+	num41()
+}
+
+func num0() {
+	i := 2
+
+	fmt.Println("Example: If condition")
+
+	if i == 0 {
+		fmt.Println("Zero")
+	} else if i == 1 {
+		fmt.Println("One")
+	} else if i == 2 {
+		fmt.Println("Two")
+	} else if i == 3 {
+		fmt.Println("Three")
+	} else {
+		fmt.Println("Your i not in case.")
+	}
+}
+
+func num1() {
+	count := 0 // เพิ่มตัวแปรนับจำนวน
+
+	for i := 1; i < 100; i++ {
+		if i%3 == 0 {
+			fmt.Print(i, ",")
+			count++ // เพิ่มตัวนับเมื่อพบตัวเลขที่หารด้วย 3 ลงตัว
+		}
+	}
+
+	fmt.Printf("\nTotal: %d\n", count) // แสดงผลรวมของจำนวนตัวเลขที่หารด้วย 3 ลงตัว
+
+}
+
+func num12() {
+	result := power(20, 2)
+	fmt.Println("20,2 =", result)
+}
+
+func num2() {
+	x := []int{48, 96, 86, 68, 57, 82, 63, 70, 37, 34, 83, 27, 19, 97, 9, 17}
+
+	minValue, maxValue := findMinMax(x)
+
+	fmt.Printf("Minimum number is: %d\n", minValue)
+	fmt.Printf("Maximum number is: %d\n", maxValue)
+}
+
+// หาเลขน้อยกว่ามากกว่า
+func findMinMax(arr []int) (int, int) {
+	if len(arr) == 0 {
+		return 0, 0
+	}
+
+	min := arr[0]
+	max := arr[0]
+
+	for _, num := range arr {
+		if num < min {
+			min = num
+		}
+		if num > max {
+			max = num
+		}
+	}
+	return min, max
+}
+
+func num3() {
+	count := countNineIn1000(1, 1000)
+	fmt.Println("Total number of 9 from 1 to 1000 =", count)
+}
+
+func countNineIn1000(start, end int) int {
+	count := 0
+	for i := start; i <= end; i++ {
+		numStr := strconv.Itoa(i)
+		for _, char := range numStr {
+			if char == '9' {
+				count++
+			}
+		}
+	}
+	return count
+}
+
+func num31() {
+	count := someFunc(10000)
+	fmt.Println("Total number of 9 from 1 to 10000 =", count)
+}
+
+func someFunc(end int) int {
+	count := 0
+	for i := 1; i <= end; i++ {
+		count += countNumber(i)
+	}
+	return count
+}
+
+func countNumber(num int) int {
+	count := 0
+	numStr := strconv.Itoa(num)
+	for _, char := range numStr {
+		if char == '9' {
+			count++
+		}
+	}
+	return count
+}
+
+func num4() {
+	var myWords = "AW SOME GO!"
+	var result string
+
+	for i := 0; i < len(myWords); i++ {
+		if myWords[i] != ' ' { // เช็คว่าตัวอักษรไม่ใช่ช่องว่าง
+			result += string(myWords[i])
+		}
+	}
+
+	fmt.Println(result)
+
+}
+
+func num41() {
+	text := "ine t"
+	result := cutText(text)
+	fmt.Println("Result:", result)
+}
+
+func cutText(input string) string {
+	return strings.ReplaceAll(input, " ", "")
+}
+
+func spacial() {
+	fmt.Println("*")
+	fmt.Println("**")
+	fmt.Println("***")
+	fmt.Println("****")
+	fmt.Println("*****")
+	fmt.Println("******")
+}
